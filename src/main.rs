d06573da6663e9b07d2e009f9f1a8424424a01b9
Dengjianping <djptux@gmail.com>
2020-04-12 23:01:01 +0800

    add example showing how to use protocol gossipsub

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod p2p_ping;
 mod p2p_chat;
+mod p2p_gossipsub;
 
 #[tokio::main]
 async fn main() {
     // let _ = p2p_ping::p2p_ping().await;
-    let _ = p2p_chat::p2p_chat().await;
+    // let _ = p2p_chat::p2p_chat().await;
+    let _ = p2p_gossipsub::gossipsub_chat().await;
 }
